ee5c9d76d682c62296f269b54a4ad94187f73344
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-03-11 18:51:09 -0400

    Models Completed

--- eventsApp/Models/Attendee.swift
+++ eventsApp/Models/Attendee.swift
@@ -5,3 +5,31 @@
 //  Created by Vinaydeep Singh Padda on 2025-03-11.
 //
 
+import Foundation
+
+struct Attendee: Identifiable, Equatable {
+    let id: UUID
+    var firstName: String
+    var lastName: String
+    var avatar: String
+    var isHost: Bool
+    
+    
+    
+    init(id: UUID = UUID(), firstName: String = "", lastName: String = "", avatar: String = "person.circle", isHost: Bool = false) {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.avatar = avatar
+        self.isHost = isHost
+    }
+}
+
+
+extension Attendee {
+    static let sample = Attendee(
+        firstName: "Neha",
+        lastName: "Sareen",
+        avatar: "person.crop.circle"
+    )
+}
--- eventsApp/Models/Host.swift
+++ eventsApp/Models/Host.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+
+struct Host: Identifiable, Equatable {
+    let id: UUID
+    var firstName: String
+    var lastName: String
+    var avatar: String
+    var isHost: Bool = true
+    
+    
+    init(id: UUID = UUID(), firstName: String = "", lastName: String = "", avatar: String = "person.circle"){
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+        self.avatar = avatar
+    }
+}
+
+extension Host {
+    static let sample = Host(
+        firstName: "Vinaydeep Singh",
+        lastName: "Padda",
+        avatar: "person.circle"
+    )
+}
