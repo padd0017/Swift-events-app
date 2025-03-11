a23ab6ff4d22de17f85dd9bceff1dede618cb0a0
Vinaydeep Singh Padda <vinaydeepsinghpadda@Vinaydeeps-MacBook-Pro.local>
2025-03-11 18:31:50 -0400

    Assignment Started

---
+++ eventsApp/eventsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  eventsAppApp.swift
+//  eventsApp
+//
+//  Created by Vinaydeep Singh Padda on 2025-03-11.
+//
+
+import SwiftUI
+
+@main
+struct eventsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eventsAppTests/eventsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  eventsAppTests.swift
+//  eventsAppTests
+//
+//  Created by Vinaydeep Singh Padda on 2025-03-11.
+//
+
+import Testing
+@testable import eventsApp
+
+struct eventsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
